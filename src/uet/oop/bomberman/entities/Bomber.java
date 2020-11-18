76c10d11e5464b6e7a75918bf05aa69c0558a7a3
huyenbui117 <62584585+huyenbui117@users.noreply.github.com>
2020-11-18 21:28:39 +0700

    added bomber animation

--- src/uet/oop/bomberman/entities/Bomber.java
+++ src/uet/oop/bomberman/entities/Bomber.java
@@ -1,13 +1,11 @@
 package uet.oop.bomberman.entities;
 
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import uet.oop.bomberman.BombermanGame;
-import uet.oop.bomberman.entities.tiles.Wall;
 import uet.oop.bomberman.graphics.Sprite;
-
-import java.util.Timer;
 
 public class Bomber extends Entity {
     public static final Coordinate UP = new Coordinate(0, -1);
@@ -17,7 +15,19 @@ public class Bomber extends Entity {
 
     public static final Coordinate IDLE = new Coordinate(0, 0);
     private final Coordinate dir;
-    private double speed = 0.5;
+    private boolean isMoving=false;
+    private double speed = 0.1;
+
+    protected int animate = 0;
+    protected final int MAX_ANIMATE = 7500;
+
+    /**
+     * Hoat hinh
+     */
+    protected void animate() {
+        if(animate < MAX_ANIMATE) animate++;
+        else animate = 0;
+    }
 
     public Bomber(Coordinate pos, Image img) {
         super(pos, img);
@@ -26,22 +36,22 @@ public class Bomber extends Entity {
 
     @Override
     public void update() {
+        animate();
         move();
     }
-    public boolean movable(){
+    protected boolean movable(){
         Coordinate tmp=pos.add(dir.multiple(speed));
         if (tmp.y == 0 || tmp.y == BombermanGame.HEIGHT - 1 || tmp.x == 0 || tmp.x == BombermanGame.WIDTH - 1) {
             return false;
         }
         return true;
     }
-    public void move() {
+    protected void move() {
         if (movable()) pos=pos.add(dir.multiple(speed));
-
     }
 
     @Override
-    public void setDir(Coordinate dir) {
+    protected void setDir(Coordinate dir) {
         this.dir.setTo(dir);
     }
 
@@ -62,6 +72,7 @@ public class Bomber extends Entity {
             } else if (event.getCode() == KeyCode.DOWN) {
                 dir.y=0;
             }
+            isMoving=false;
         }
         else if(event.getEventType().equals(KeyEvent.KEY_PRESSED)){
             if (event.getCode() == KeyCode.RIGHT) {
@@ -73,6 +84,42 @@ public class Bomber extends Entity {
             } else if (event.getCode() == KeyCode.DOWN) {
                 dir.setTo(DOWN);
             }
+            isMoving=true;
         }
+    }
+    private void chooseSprite() {
+        if (dir.equalTo(UP)) {
+            sprite = Sprite.player_up;
+            if (isMoving) {
+                sprite = Sprite.movingSprite(Sprite.player_up_1, Sprite.player_up_2, animate, 20);
+            }
+        } else if (dir.equalTo(RIGHT)) {
+            sprite = Sprite.player_right;
+            if (isMoving) {
+                sprite = Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, animate, 20);
+            }
+        } else if (dir.equalTo(DOWN)) {
+            sprite = Sprite.player_down;
+            if (isMoving) {
+                sprite = Sprite.movingSprite(Sprite.player_down_1, Sprite.player_down_2, animate, 20);
+            }
+        } else if (dir.equalTo(LEFT)) {
+            sprite = Sprite.player_left;
+            if (isMoving) {
+                sprite = Sprite.movingSprite(Sprite.player_left_1, Sprite.player_left_2, animate, 20);
+            }
+        } else {
+            sprite = Sprite.player_right;
+            if (isMoving) {
+                sprite = Sprite.movingSprite(Sprite.player_right_1, Sprite.player_right_2, animate, 20);
+            }
+        }
+        img=sprite.getFxImage();
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        chooseSprite();
+        gc.drawImage(img, pos.x * Sprite.SCALED_SIZE, pos.y * Sprite.SCALED_SIZE);
     }
 }
